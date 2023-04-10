a85f8626f7090889872860962bc921b57d7a91e2
victor <victor@compubility.com>
2023-04-10 17:07:08 -0400

    fix credits container direction

--- src/pages/credits.tsx
+++ src/pages/credits.tsx
@@ -5,7 +5,7 @@ import Grid from '@mui/material/Unstable_Grid2';
 export const CreditsPage = () =>{
 
     return (
-        <Grid container>
+        <Grid container direction="column">
             <Grid m={2}>
                     <Typography variant="h5">
                         Purpose
